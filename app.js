afdadec21dc2d891933359a0cefbd497477460f9
Daedra <foglzerika@gmail.com>
2020-03-18 22:01:52 -0600

    feat | Create routes, model and configure server to create /signup endpoint

--- app.js
+++ app.js
@@ -1,47 +1,92 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
+require('dotenv').config();
 
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const cors         = require('cors');
+
+
+// WHEN INTRODUCING USERS DO THIS:
+// INSTALL THESE DEPENDENCIES: passport-local, passport, bcryptjs, express-session
+// AND UN-COMMENT OUT FOLLOWING LINES:
+
+const session       = require('express-session');
+const passport      = require('passport');
+
+require('./configs/passport');
+
+// IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
+
+mongoose
+     .connect(`${process.env.DB_URI}`, {useNewUrlParser: true})
+
+    // .connect('mongodb://localhost/crypto-wallet-server', {useNewUrlParser: true})
+    // .connect(`mongodb+srv://erika:${process.env.DB_PASS}@cluster0-mzjwr.mongodb.net/crypto-wallet-server?retryWrites=true&w=majority`, {useNewUrlParser: true})
+    .then(x => {
+      console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+      console.error('Error connecting to mongo', err)
+    });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// ADD SESSION SETTINGS HERE:
+app.use(session({
+    secret:"some secret goes here",
+    resave: true,
+    saveUninitialized: true
+}));
+
+// USE passport.initialize() and passport.session() HERE:
+app.use(passport.initialize());
+app.use(passport.session());
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+// ADD CORS SETTINGS HERE TO ALLOW CROSS-ORIGIN INTERACTION:
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
+
+
+
+// ROUTES MIDDLEWARE STARTS HERE:
 
 const index = require('./routes/index');
 app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+const authRoutes = require('./routes/auth-routes');
+app.use('/api', authRoutes);
 
 module.exports = app;
---
+++ models/user-model.js
@@ -0,0 +1,15 @@
+ const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username :{type: String , required: true , maxlength:20, trim:true},
+    password :{ type: String, required: true, maxlength: 100, trim : true},
+    image: {type: String}
+
+},
+{
+    timestamps: {createdAt: "created_at" , updatedAt : "updated_at"}
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
